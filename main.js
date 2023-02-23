8e6779847ec0cf3f6444524c234de0acc11a6686
douglasgamez <douglasgamez2016@gmail.com>
2023-02-23 10:24:02 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,76 +1,65 @@
-// CHART START
 
-// Definicion de constantes
+// Constantes
 const width = 800
 const height = 500
-const margin = {top: 10,    
-    bottom: 40,    
-    left: 10,     
+const margin = {
+    top: 10,
+    bottom: 20,
+    left: 60,
     right: 20
 }
 
-// Declaracion del svg
+//Variable global
+let data_global
+
+
+// Grupos
 const svg = d3.select("#chart")
-.append("svg")
-.attr("id", "svg")
-.attr("width", width)
-.attr("height", height)
-
-
+    .append("svg")
+    .attr("width",width)
+    .attr("height", height)
 const elementGroup = svg.append("g")
-.attr("id", "elementGroup")
-.attr('transform', `translate(${0}, ${margin.top})`)
+    .attr("id","elementgroup")
+    .attr("transform",`translate(${margin.left}, ${margin.top})`)
+const axisGroup = svg.append("g")
+    .attr("id","axisGroup")
+const xAxisGroup = axisGroup.append("g")
+    .attr("id","xAxis")
+    .attr("transform",`translate(${margin.left}, ${height - margin.bottom})`)
+const yAxisGroup = axisGroup
+    .append("g").attr("id","yAxis")
+    .attr("transform",`translate(${margin.left}, ${margin.top})`)
+ 
+// escalas y ejes 
+ const x = d3.scaleLinear().range([0, width - margin.left - margin.right])
+ const y = d3.scaleBand().range([height - margin.top - margin.bottom, 0]).padding(0.1)
 
+const xAxis = d3.axisBottom().scale(x)
+const yAxis = d3.axisLeft().scale(y)
 
-// Declaracion de las escalas:
-const y = d3.scaleBand()
-    .range([0 , height - margin.top - margin.bottom])
-    .paddingInner(0.2).paddingOuter(0.04)
-const x = d3.scaleLinear()
-    .range([0, width - margin.left - margin.right])
-
-// Declaracion de los ejes
-const axisGroup = svg.append("g").attr("id", "axisGroup")
-const xAxisGroup = axisGroup.append("g").attr("id", "xAxisGroup").attr('transform', `translate(${margin.left},${height - margin.bottom})`)
-const yAxisGroup = axisGroup.append("g").attr("id", "yAxisGroup").attr('transform', `translate(${margin.left},${margin.top})`)
-
-const dateFormat = d3.timeParse("%Y")
-
-const xAxis = d3.axisBottom()
-    .scale(x).ticks(5).tickSize(-height)
-const yAxis = d3.axisLeft()
-    .scale(y).ticks(9).tickSize(-width)
-
-
-
-// Formato de datos
+// data
 d3.csv("data.csv").then(data => {
-    data = d3.nest()
-        .key(d => d.winner)
-        .entries(data)
-    
     data.map(d => {
-        d.title = d.values.length
-        d.winner = d.key
-        d.year = + d.year
-    })
-
-    // Añadir el dominio a la escala 
-    y.domain(data.map(d=> d.key))
-    x.domain(d3.extent(data.map(d=> d.title)))
-
-    // Llamar a los ejes
-    xAxisGroup.call(xAxis)
-    yAxisGroup.call(yAxis)
-
-    //Data binding
-    let bars = elementGroup.selectAll("rect").data(data)
+        d.year = +d.year})
+        data = d3.nest()
+            .key(d => d.winner)
+            .entries(data.filter(d => d.winner != ''))
+    data_global = data
     
-    // Patron actualizacion
-    bars.enter().append('rect')
-        .attr('class', "bar")
-        .attr('height', y.bandwidth())
-        .attr('width',d=> width - x(d.values.length) - margin.left - margin.right )
-        .attr('x', d => x(d.values.length))
-        .attr('y', d => y(d.key))
+// dominio y ejes    
+x.domain([0, d3.max(data_global.map(d => d.values.length)),10])
+y.domain(data_global.map(d => d.key))
+    
+xAxisGroup.call(xAxis)
+yAxisGroup.call(yAxis)
+
+// data binding
+elementGroup.selectAll("rect").data(data_global)
+.enter()
+.append("rect")
+    .attr("x", 0)
+    .attr("y", (d) => y(d.key))
+    .attr("width", d => x(d.values.length))
+    .attr("height", y.bandwidth())
+    .attr("class", d => d.key)
 })
